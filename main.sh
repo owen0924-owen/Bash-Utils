48ff2b991b91540ee7c40a988ac36e00681f6283
owen0924 <64713824+owen0924-owen@users.noreply.github.com>
2023-01-24 17:37:07 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,37 @@
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+clear
+
+UpdatePterodactylPanel(){
+    bash <(curl https://raw.githubusercontent.com/owen0924-owen/Pterodactyl-Panel-Update-Script/main/updatepanel.sh)
+}
+FreeTheMemory(){
+    bash <(curl https://raw.githubusercontent.com/owen0924-owen/free-the-memory-script/main/free.sh)
+}
+}
+echo "Copyright (c) 2023 SHD Cloud | owen0924"
+echo "Choose an option down below to execute specify action."
+echo ""
+echo "Discord: owen0924#9262"
+echo "Website: https://shdhost.xyz/"
+echo ""
+echo "[1] Free The Memory"
+echo "[2] Update Pterodactyl Panel"
+echo "[3] Exit"
+
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    FreeTheMemory
+fi
+if [ $choice == "2" ]
+    then
+    UpdatePterodactylPanel
+fi
+if [ $choice == "3" ]
+    then
+    exit
+fi
